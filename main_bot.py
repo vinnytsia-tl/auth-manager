7e1ceec59f72c60670afbef14d47373f1b864a9e
Kostiantyn Kostiuk <kostyanf14@live.com>
2023-03-08 19:59:02 +0200

    Deduplicate logger
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- main_bot.py
+++ main_bot.py
@@ -1,31 +1,11 @@
-import os
-import logging
-
 from app.bot import Bot
 from app.config import Config
 from app.database.migrations import apply_migrations
-
-
-def setup_app_logger():
-    logger = logging.getLogger('app')
-    logger.setLevel(Config.log_level)
-    formatter = logging.Formatter('[%(asctime)s] #%(thread)d [%(levelname)s] %(name)s: %(message)s', datefmt='%d/%b/%Y:%H:%M:%S')
-
-    # create a StreamHandler to log to the console
-    console_handler = logging.StreamHandler()
-    console_handler.setLevel(Config.log_level)
-    console_handler.setFormatter(formatter)
-    logger.addHandler(console_handler)
-
-    # create a FileHandler to log to a file
-    file_handler = logging.FileHandler(os.path.join(Config.log_directory, 'app_bot.log'))
-    file_handler.setLevel(Config.log_level)
-    file_handler.setFormatter(formatter)
-    logger.addHandler(file_handler)
+from app.logging import setup_app_logger
 
 
 if __name__ == '__main__':
     Config.load()
-    setup_app_logger()
+    setup_app_logger('app_bot.log')
     apply_migrations()
     Bot().start()
