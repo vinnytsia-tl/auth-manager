2df509b61248b19ef94e3e1f67692e77be13d360
Kostiantyn Kostiuk <kostyanf14@live.com>
2023-03-17 23:32:29 +0200

    LDAP add exceptions
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- app/system/ldap/ldap.py
+++ app/system/ldap/ldap.py
@@ -1,10 +1,23 @@
 import logging
+from typing import Tuple
 
 import ldap3
 
 from .config import LDAPConfig
 
 logger = logging.getLogger(__name__)
+
+
+class LDAPException(Exception):
+    pass
+
+
+class LDAPBindException(LDAPException):
+    pass
+
+
+class LDAPSearchException(LDAPException):
+    pass
 
 
 class LDAP():
@@ -16,18 +29,18 @@ class LDAP():
         self.server = ldap3.Server(
             config.ldap_server, config.ldap_port, use_ssl=config.ldap_use_ssl)
 
-    def set_password(self, username, password):
+    def set_password(self, username, password) -> bool:
         conn = ldap3.Connection(self.server, user=self.config.admin_user,
                                 password=self.config.admin_password, version=3)
         if not conn.bind():
             logger.error('LDAP bind failed with user %s',
                          self.config.admin_user)
-            return None
+            raise LDAPBindException(f"LDAP bind failed with user {self.config.admin_user}")
 
         _, _, distinguished_name = self.getuser(username)
         return conn.extend.microsoft.modify_password(distinguished_name, password)
 
-    def getuser(self, user_principal_name, search_base=None):
+    def getuser(self, user_principal_name, search_base=None) -> Tuple[str, str, str]:
         if search_base is None:
             search_base = self.config.user_base
 
@@ -36,7 +49,7 @@ class LDAP():
         if not conn.bind():
             logger.error('LDAP bind failed with user %s',
                          self.config.admin_user)
-            return None
+            raise LDAPBindException(f"LDAP bind failed with user {self.config.admin_user}")
 
         user_principal_name = self.normalize_login(user_principal_name)
         search_filter = f'(&(objectCategory=person)(objectClass=user)(userPrincipalName={user_principal_name}))'
@@ -44,7 +57,7 @@ class LDAP():
         if not conn.search(search_base, search_filter, attributes=attrs):
             logger.error(
                 'No LDAP entries found for search base %s and filter %s', search_base, search_filter)
-            return None
+            raise LDAPSearchException(f"No LDAP entries found for search base {search_base} and filter {search_filter}")
 
         entry = conn.entries[0]
         if entry is not None:
@@ -54,9 +67,9 @@ class LDAP():
 
         logger.warning('No LDAP entry found for user %s in search base %s',
                        user_principal_name, search_base)
-        return None
+        raise LDAPSearchException(f"No LDAP entry found for user {user_principal_name} in search base {search_base}")
 
-    def login(self, username, password):
+    def login(self, username, password) -> bool:
         if username is None or username == '' or password is None or password == '':
             logger.warning(
                 "LDAP login without username/password is unsupported")
