bd776b070a9bdb490ae06b74fcdf8e57bc0d06ad
Kostiantyn Kostiuk <kostyanf14@live.com>
2023-03-18 16:53:26 +0200

    Add web svc
    Signed-off-by: Kostiantyn Kostiuk <kostyanf14@live.com>

--- main_web.py
+++ main_web.py
@@ -3,8 +3,13 @@ from app.database.migrations import apply_migrations
 from app.logging import setup_app_logger
 from app.web import Web
 
-if __name__ == '__main__':
+
+def main_web():
     Config.load()
     setup_app_logger('app_web.log')
     apply_migrations()
     Web.start()
+
+
+if __name__ == '__main__':
+    main_web()
